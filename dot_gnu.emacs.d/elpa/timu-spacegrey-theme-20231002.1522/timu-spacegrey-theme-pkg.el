617423191d22d6c6d4717703734866d590471eca
Cass Dahle <isaacbd.personal@gmail.com>
2023-10-07 22:49:59 -0700

    Adding my whole GNU emacs config

---
+++ dot_gnu.emacs.d/elpa/timu-spacegrey-theme-20231002.1522/timu-spacegrey-theme-pkg.el
@@ -0,0 +1,14 @@
+(define-package "timu-spacegrey-theme" "20231002.1522" "Color theme inspired by the Spacegray theme in Sublime Text"
+  '((emacs "26.1"))
+  :commit "145ee85b9d65bbef32687681d65f72e227e52225" :authors
+  '(("Aimé Bertrand" . "aime.bertrand@macowners.club"))
+  :maintainers
+  '(("Aimé Bertrand" . "aime.bertrand@macowners.club"))
+  :maintainer
+  '("Aimé Bertrand" . "aime.bertrand@macowners.club")
+  :keywords
+  '("faces" "themes")
+  :url "https://gitlab.com/aimebertrand/timu-spacegrey-theme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
